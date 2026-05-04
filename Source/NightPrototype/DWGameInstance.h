e25e0b3c94657f6cbb5f16ab0194909021cfa5a1
manss123 <48107845+manss123@users.noreply.github.com>
2026-05-04 15:50:04 +0700

    Stable Memory v.01

---
+++ Source/NightPrototype/DWGameInstance.h
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "DWGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NIGHTPROTOTYPE_API UDWGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "World Memory")
+	TSet<FName> WorldEventTags;
+	
+public:
+	static const FName Event_PlayerInspectedCorpse;
+	static const FName Event_PlayerReadNoticeBoard;
+	
+	UFUNCTION(BlueprintCallable, Category = "World Memory")
+	void AddWorldEvent(FName EventTag);
+	
+	UFUNCTION(BlueprintPure, Category = "World Memory")
+	bool HasWorldEvent(FName EventTag) const;
+	
+	UFUNCTION(BlueprintCallable, Category = "World Memory")
+	void ClearWorldEvents();
+	
+	UFUNCTION(BlueprintCallable, Category = "World Memory|Events")
+	void MarkPlayerInspectedCorpse();
+	
+	UFUNCTION(BlueprintPure, Category = "World Memory|Events")
+	bool HasPlayerInspectedCorpse() const;
+	
+	UFUNCTION(BlueprintCallable, Category = "World Memory|Events")
+	void MarkPlayerReadNoticeBoard();
+	
+	UFUNCTION(BlueprintPure, Category = "World Memory|Events")
+	bool HasPlayerReadNoticeBoard() const;
+	
+	UFUNCTION(BlueprintPure, Category = "World Memory|Debug")
+	int32 GetWorldEventCount() const;
+	
+	UFUNCTION(BlueprintPure, Category = "World Memory|Debug")
+	TArray<FName> GetWorldEvents() const;
+	
+	UFUNCTION(BlueprintCallable, Category = "World Memory|Debug")
+	void PrintWorldEvents() const;
+};
